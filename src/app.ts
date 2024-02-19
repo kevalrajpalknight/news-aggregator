7ebeef08d8d931ecb305da341db985d46123c925
KevalRajpal-Digimonk <keval.rajpal@digimonk.in>
2024-02-19 12:00:25 +0530

    add utility middlewares

---
+++ src/app.ts
@@ -0,0 +1,30 @@
+import cors from 'cors';
+import dotenv from 'dotenv';
+import express from 'express';
+import helmet from 'helmet';
+import morgan from 'morgan';
+
+// Initialize configuration
+dotenv.config();
+
+// Initialize express server configuration
+const app = express();
+const port = process.env["PORT"] || 8000;
+
+// Express configuration
+// Express configuration
+app.use(cors()); // Enable CORS
+app.use(helmet()); // Enable Helmet
+app.use(morgan('dev')); // Enable Morgan
+app.use(express.json()); // Enable JSON body parser
+app.use(express.urlencoded({ extended: true }));
+
+
+// Start Express server
+app.listen(port, () => {
+  // Callback function when server is successfully started
+  console.log(`Server started at http://localhost:${port}`);
+});
+
+// Export Express app
+module.exports = app;
