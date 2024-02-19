774b5823a082e29589644741dceda529e50a9084
KevalRajpal-Digimonk <keval.rajpal@digimonk.in>
2024-02-19 13:16:23 +0530

    create database connection

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,9 @@ import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
 import helmet from "helmet";
+import mongoose from "mongoose";
+
+import Logger from "./utils/logger";
 import morganMiddleware from "./utils/middleware/morganMiddleware";
 
 // Initialize configuration
@@ -18,6 +21,20 @@ app.use(helmet()); // Enable Helmet
 app.use(morganMiddleware);
 app.use(express.json()); // Enable JSON body parser
 app.use(express.urlencoded({ extended: true }));
+
+if (process.env.NODE_ENV != "test") {
+  try {
+    const database_uri = process.env.DATABASE_URI;
+    if (!database_uri) {
+      throw Error("Could not find the database URI");
+    } else {
+      mongoose.connect(database_uri);
+      Logger.info("Connected to the db");
+    }
+  } catch (err) {
+    Logger.error(err._message);
+  }
+}
 
 // Start Express server
 app.listen(port, () => {
