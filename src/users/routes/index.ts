1abb93f0fd43f8861b201bade8c1d847b9192192
KevalRajpal-Digimonk <keval.rajpal@digimonk.in>
2024-02-19 18:28:15 +0530

    refactor users and remove auth

--- src/users/routes/index.ts
+++ src/users/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { fetchAllPreferences } from "../controllers";
-
+import { fetchAllPreferences, signIn, signUp } from "../controllers";
+import { validateSignInSchema, validateSignUpSchema } from "../validators";
 const router = Router();
 
+router.post("/signup", validateSignUpSchema, signUp);
+router.post("/login", validateSignInSchema, signIn);
 router.get("/preferences", fetchAllPreferences);
 
 export default router;
